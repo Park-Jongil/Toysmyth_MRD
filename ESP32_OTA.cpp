06f0e3eaab8069441aaddd42c9dd869aebe1c299
Park-Jongil <always97@nate.com>
2024-05-23 16:50:52 +0900

    20240523 OTA 최신버전확인기능

--- ESP32_OTA.cpp
+++ ESP32_OTA.cpp
@@ -7,7 +7,7 @@
 //-----------------------------------------------------------------------------------------------
 // Firmware Version Check
 //-----------------------------------------------------------------------------------------------
-String  szFirmwareVersion = "20240513_01";
+String  szFirmwareVersion = "20240523_01";
 String  PRODUCT_CODE = "TOYS_MRD_001";
  
 //-----------------------------------------------------------------------------------------------
@@ -119,14 +119,16 @@ void Toysmyth_Check_FirmwareUpdate() {
     deserializeJson(get_jsondata, responseBody);
     szLastVerion = get_jsondata["VERSION"].as<String>();
     szUpdateURL = get_jsondata["BIN_PATH"].as<String>();
-    Serial.printf("  Firmware Current Version  = %s\n",szFirmwareVersion.c_str());
-    Serial.printf("  Firmware Check Version  = %s\n",szLastVerion.c_str());
+    Serial.printf("  Firmware Current Version   = %s\n",szFirmwareVersion.c_str());
+    Serial.printf("  Firmware Updatable Version = %s\n",szLastVerion.c_str());
     if (szLastVerion != NULL && szLastVerion.length() >  7) {
-      if (szFirmwareVersion != szLastVerion) {    // 현재 펌웨어버전과 다르다면
+      if (szFirmwareVersion < szLastVerion) {    // 현재 펌웨어버전과 다르다면
         Serial.printf("  Firmware Update URL = %s\n",szUpdateURL.c_str());
         for(int i=0;i<3;i++) performOTAUpdate(szUpdateURL,szLastVerion);     // 총 5번의 업데이트를 시도한다.
         Serial.printf("  Firmware Update Failure\n");
         Toysmyth_FirmwareUpdate_to_Server(szLastVerion,0x01,"Firmware Update Failure");
+      } else {
+        Serial.printf("  Current version is new.\n");
       }
     }
   }
